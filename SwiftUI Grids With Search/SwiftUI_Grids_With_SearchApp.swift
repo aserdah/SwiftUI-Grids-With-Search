ead693e27bdc280fe6a88d29d7589d077ea8e420
Ahmed Moeen Serdah <35952861+aserdah@users.noreply.github.com>
2020-09-29 15:49:52 +0300

    Initial Commit

---
+++ SwiftUI Grids With Search/SwiftUI_Grids_With_SearchApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Grids_With_SearchApp.swift
+//  SwiftUI Grids With Search
+//
+//  Created by Ahmed Serdah on 29/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Grids_With_SearchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
