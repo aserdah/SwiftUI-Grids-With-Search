01b768a1894bc532480e0d65df37aa9b45faf791
Ahmed Moeen Serdah <35952861+aserdah@users.noreply.github.com>
2020-09-29 17:19:40 +0300

    in

--- SwiftUI Grids With Search/SwiftUI_Grids_With_SearchApp.swift
+++ SwiftUI Grids With Search/SwiftUI_Grids_With_SearchApp.swift
@@ -11,7 +11,9 @@ import SwiftUI
 struct SwiftUI_Grids_With_SearchApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            GridsView()
         }
     }
 }
+
+
